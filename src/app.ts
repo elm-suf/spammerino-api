a08c87bb125da383fd45578a588efeff4e5ac7f7
elm-suf <elm.sufian@gmail.com>
2025-12-14 19:35:18 +0100

    Revert "feat: add cors middleware to app initialization"
    This reverts commit edc8c92476c55b46241331c4b518cfb6b4e8eaaa.

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import createApp from "@/lib/create-app";
 import index from "@/routes/index.route";
 import users from "@/routes/users/users.index";
 import { Hono } from "hono";
-import { cors } from "hono/cors";
 
 const app = new Hono();
 const api = createApp();
@@ -21,13 +20,6 @@ routes.forEach((route) => {
 
 export type AppType = typeof routes[number];
 
-app.use(
-    '*',
-    cors({
-      origin: 'http://localhost:3000',
-      allowMethods: ['POST', 'GET', 'OPTIONS'],
-    })
-  )
 app.route("/", api);
 
 export default app;
