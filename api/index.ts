04a330a9727b20fd1e3fcee763e78792ed166e05
elm-suf <elm.sufian@gmail.com>
2025-12-14 18:17:40 +0100

    feat: add initial vercel edge configuration and api setup
    Add vercel.json for routing configuration and api/index.ts as edge function handler

---
+++ api/index.ts
@@ -0,0 +1,13 @@
+import { handle } from 'hono/vercel'
+
+// @ts-ignore
+import app from '../dist/src/app.js'
+export const runtime = 'edge'
+
+export const DELETE = handle(app);
+export const GET = handle(app);
+export const HEAD = handle(app);
+export const OPTIONS = handle(app);
+export const PATCH = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
